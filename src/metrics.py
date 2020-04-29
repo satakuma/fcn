883ae2318799f11185e1b21e4bb23f8f592f035e
tmkkk <tymenty33@gmail.com>
2020-04-29 23:18:49 +0200

    Modify metrics

--- src/metrics.py
+++ src/metrics.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import tensorflow as tf
-from tensorflow.keras.metrics import MeanIoU, sparse_categorical_accuracy
+from tensorflow.keras.metrics import MeanIoU, Accuracy
 
 
 class VoidPixelMeanIoU(MeanIoU):
@@ -10,12 +10,15 @@ class VoidPixelMeanIoU(MeanIoU):
     
     def update_state(self, y_true, y_pred, sample_weight=None):
         y_pred = tf.math.argmax(y_pred, axis=-1)
+
         if self.void_pixel is not None:
             y_true = tf.reshape(y_true, [-1])
             y_pred = tf.reshape(y_pred, [-1])
+
             mask = tf.math.not_equal(y_true, self.void_pixel)
             y_true = tf.boolean_mask(y_true, mask)
             y_pred = tf.boolean_mask(y_pred, mask)
+        
         super(VoidPixelMeanIoU, self).update_state(y_true, y_pred, sample_weight)
 
 
@@ -26,17 +29,20 @@ class ArgmaxMeanIoU(MeanIoU):
     def update_state(self, y_true, y_pred, sample_weight=None):
         super(ArgmaxMeanIoU, self).update_state(y_true, tf.math.argmax(y_pred, axis=-1), sample_weight)
 
-def sparse_categorical_accuracy_void_pixel(y_true, y_pred):
 
-
-class SparseCategoricalAccuracyVoidPixel:
-    def __init__(self, void_pixel):
+class VoidPixelAccuracy(Accuracy):
+    def __init__(self, *args, void_pixel, **kwargs):
+        super(VoidPixelAccuracy, self).__init__(*args, **kwargs)
         self.void_pixel = void_pixel
     
-    def __call__(self, y_true, y_pred):
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.math.argmax(y_pred, axis=-1)
+
         y_true = tf.reshape(y_true, [-1])
         y_pred = tf.reshape(y_pred, [-1])
+
         mask = tf.math.not_equal(y_true, self.void_pixel)
         y_true = tf.boolean_mask(y_true, mask)
         y_pred = tf.boolean_mask(y_pred, mask)
-        return sparse_categorical_accuracy(y_true, y_pred)
+
+        super(VoidPixelAccuracy, self).update_state(y_true, y_pred, sample_weight)
