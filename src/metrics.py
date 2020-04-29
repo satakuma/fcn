529fc49d6a5380d7a9f43de916e269204635c392
tmkkk <tymenty33@gmail.com>
2020-04-29 22:41:14 +0200

    Add reshaping

--- src/metrics.py
+++ src/metrics.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import tensorflow as tf
-from tensorflow.keras.metrics import MeanIoU
+from tensorflow.keras.metrics import MeanIoU, sparse_categorical_accuracy
+
 
 class VoidPixelMeanIoU(MeanIou):
     def __init__(self, *args, void_pixel=None, **kwargs):
@@ -10,10 +11,13 @@ class VoidPixelMeanIoU(MeanIou):
     def update_state(self, y_true, y_pred, sample_weight=None):
         y_pred = tf.math.argmax(y_pred, axis=-1)
         if self.void_pixel is not None:
+            y_true = tf.reshape(y_true, [-1])
+            y_pred = tf.reshape(y_pred, [-1])
             mask = tf.math.not_equal(y_true, self.void_pixel)
             y_true = tf.boolean_mask(y_true, mask)
             y_pred = tf.boolean_mask(y_pred, mask)
         super(ArgmaxMeanIoU, self).update_state(y_true, y_pred, sample_weight)
+
 
 class ArgmaxMeanIoU(MeanIoU):
     def __init__(self, *args, **kwargs):
@@ -22,13 +26,15 @@ class ArgmaxMeanIoU(MeanIoU):
     def update_state(self, y_true, y_pred, sample_weight=None):
         super(ArgmaxMeanIoU, self).update_state(y_true, tf.math.argmax(y_pred, axis=-1), sample_weight)
 
+
 class SparseCategoricalAccuracyVoidPixel:
     def __init__(self, void_pixel):
         self.void_pixel = void_pixel
     
     def __call__(self, y_true, y_pred):
+        y_true = tf.reshape(y_true, [-1])
+        y_pred = tf.reshape(y_pred, [-1])
         mask = tf.math.not_equal(y_true, self.void_pixel)
         y_true = tf.boolean_mask(y_true, mask)
         y_pred = tf.boolean_mask(y_pred, mask)
-        return sparse_categorical_accuracy_void_pixel(y_true, y_pred)
-
+        return sparse_categorical_accuracy(y_true, y_pred)
